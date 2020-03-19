796798629ba138b1866139cd3afec844c46048b3
hongr <hongren24@163.com>
2020-03-19 15:35:26 +0800

    springboot+jsp

---
+++ src/main/java/com/flyride/star/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.flyride.star.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @description:
+ * @author: Mr.Wu
+ * @create: 2020/3/19 15:15
+ **/
+@Controller
+public class PageController {
+    @RequestMapping("/{page}")
+    public String getPage(@PathVariable String page){
+        return page;
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hongr
+  Date: 2020/3/19
+  Time: 15:20
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello jsp</title>
+</head>
+<body>
+    <h1>    jsp是很好玩的！     -老陈</h1>
+</body>
+</html>
